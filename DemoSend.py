58ee5c99c2d34bb6cb302de89432498a899092cf
LucasAlva <lucasganoza@gmail.com>
2017-01-28 11:12:06 -0500

    Add files via upload

--- DemoSend.py
+++ DemoSend.py
@@ -2,34 +2,24 @@ import cv2
 import numpy as np
 import pika
 import sys
-#credentials = pika.PlainCredentials('username', 'password')
-#parameters = pika.ConnectionParameters('172.20.36.177', credentials=credentials)
+credentials = pika.PlainCredentials('demo', 'demo')
+parameters = pika.ConnectionParameters('172.20.93.234',  5672, '/', credentials=credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+channel.queue_declare(queue = 'q1')
 
-#host_input_name = raw_input("Please Connect to a Host ")
-#pika.ConnectionParameters(host = host_input_name)
-#Sends a message
-def send(msg):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue = 'dim')
-    channel.basic_publish(exchange = '',  routing_key = 'dim',  body = msg)
-    
 #Camera
 cam1 = cv2.VideoCapture(0)
 
-#Counter
-c = 0
-
-#Main Loop
-while(True):
+#Sends a message
+def  on_request(ch,  method, props,  body):
     #Camera One Updates
     ret,  frame1 = cam1.read()
-    #cam_one_raw = cv2.cvtColor(frame1,  cv2.COLOR_BGR2RGB)
     img = cv2.imencode('.png',  frame1)[1].tostring()
-    send(img)
-    #cv2.imwrite("Sent"+str(c)+".png",  frame1)
-    c += 1
-    key = cv2.waitKey(20)
-    #if key == 27: # exit on ESCAPE
-        #connection.close()
-        #break
+    ch.basic_publish(exchange = '',  routing_key = props.reply_to,  properties = pika.BasicProperties(correlation_id = \
+                                                                                                                                                                                            props.correlation_id),  body = img)
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+    channel.basic_qos(prefetch_count = 1)
+    channel.basic_consume(on_request,  queue = 'q1')
+    
+channel.start_consuming()
